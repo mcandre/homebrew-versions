23ee74c04313e20e1c486d896d3c064bb38f8cc1
Andrew Pennebaker <andrew.pennebaker@networkedinsights.com>
2015-03-23 09:36:08 -0500

    archive boot2docker 1.3.3 in homebrew/versions

---
+++ boot2docker133.rb
@@ -0,0 +1,55 @@
+require "formula"
+
+class Boot2docker < Formula
+  homepage "https://github.com/boot2docker/boot2docker-cli"
+  # Boot2docker and docker are generally updated at the same time.
+  # Please update the version of docker too
+  url "https://github.com/boot2docker/boot2docker-cli.git", :tag => "v1.3.3"
+  head "https://github.com/boot2docker/boot2docker-cli.git"
+
+  bottle do
+    sha1 "67313ebf6f2cd0653d7f017ae783e92da1c9e44f" => :yosemite
+    sha1 "68fe92df4290f9494ab6ce10cfe37e5b154b2abc" => :mavericks
+    sha1 "ccdf2d4ed162f2bb49141e89853933dc8b64e9a1" => :mountain_lion
+  end
+
+  depends_on "docker" => :recommended
+  depends_on "go" => :build
+
+  def install
+    (buildpath + "src/github.com/boot2docker/boot2docker-cli").install Dir[buildpath/"*"]
+
+    cd "src/github.com/boot2docker/boot2docker-cli" do
+      ENV["GOPATH"] = buildpath
+      system "go", "get", "-d"
+
+      ENV["GIT_DIR"] = cached_download/".git"
+      system "make", "goinstall"
+    end
+
+    bin.install "bin/boot2docker-cli" => "boot2docker"
+  end
+
+  def plist; <<-EOS.undent
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+      <key>Label</key>
+      <string>#{plist_name}</string>
+      <key>ProgramArguments</key>
+      <array>
+        <string>#{opt_bin}/boot2docker</string>
+        <string>up</string>
+      </array>
+      <key>RunAtLoad</key>
+      <true/>
+    </dict>
+    </plist>
+    EOS
+  end
+
+  test do
+    system "#{bin}/boot2docker", "version"
+  end
+end
